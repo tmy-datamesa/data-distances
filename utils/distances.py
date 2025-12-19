ff5748788b9eda16707d5fc7a7627fdf84eb365e
tmy-datamesa <tmytrhn00@hotmail.com>
2025-12-19 14:40:42 +0300

    feat(utils): implement manhattan and euclidean using minkowski

--- utils/distances.py
+++ utils/distances.py
@@ -1,1 +1,14 @@
-pass  # YOUR CODE HERE
+"""Distance calculation functions."""
+import math
+
+def minkowski(a, b, p):
+    """Genel mesafe formülü (Minkowski)"""
+    d_x = abs(b[0] - a[0])
+    d_y = abs(b[1] - a[1])
+    return (d_x**p + d_y**p)**(1/p)
+
+def manhattan(a, b):
+    return minkowski(a, b, 1) # p=1 ise Manhattan olur
+
+def euclidean(a, b):
+    return minkowski(a, b, 2) # p=2 ise Euclidean olur
